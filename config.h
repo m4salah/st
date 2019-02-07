74834fe1fb809bb1e7130b8c000260df1f2c33ca
Kim Bradley <kim.jamie.bradley@gmail.com>
2019-02-07 21:52:21 +0000

    Replace default colorscheme with monokai
    Signed-off-by: Kim Bradley <kim.jamie.bradley@gmail.com>

--- config.h
+++ config.h
@@ -63,7 +63,7 @@ static unsigned int cursorthickness = 2;
 static int bellvolume = 0;
 
 /* default TERM value */
-char *termname = "st-256color";
+char *termname = "xterm-256color";
 
 /*
  * spaces per tab
@@ -86,37 +86,61 @@ unsigned int tabspaces = 8;
 unsigned int alpha = 0xed;
 
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
-	[255] = 0,
-	/* more colors can be added after 255 to use with DefaultXX */
-	"black",   /* 256 -> bg */
-	"white",   /* 257 -> fg */
+  "#1d1f21", /* base00 */
+  "#f92672", /* base08 */
+  "#a6e22e", /* base0B */
+  "#f4bf75", /* base0A */
+  "#66d9ef", /* base0D */
+  "#ae81ff", /* base0E */
+  "#a1efe4", /* base0C */
+  "#f8f8f2", /* base05 */
+  "#75715e", /* base03 */
+  "#fd971f", /* base09 */
+  "#383830", /* base01 */
+  "#49483e", /* base02 */
+  "#a59f85", /* base04 */
+  "#f5f4f1", /* base06 */
+  "#cc6633", /* base0F */
+  "#f9f8f5", /* base07 */
 };
+
+unsigned int defaultfg = 7;
+unsigned int defaultbg = 0;
+static unsigned int defaultcs = 13;
+static unsigned int defaultrcs = 0;
+
+//static const char *colorname[] = {
+//	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+//	"#cc241d",
+//	"#98971a",
+//	"#d79921",
+//	"#458588",
+//	"#b16286",
+//	"#689d6a",
+//	"#a89984",
+//	"#928374",
+//	"#fb4934",
+//	"#b8bb26",
+//	"#fabd2f",
+//	"#83a598",
+//	"#d3869b",
+//	"#8ec07c",
+//	"#ebdbb2",
+//	[255] = 0,
+	/* more colors can be added after 255 to use with DefaultXX */
+//	"black",   /* 256 -> bg */
+//	"white",   /* 257 -> fg */
+//};
 
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 15;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 15;
-static unsigned int defaultrcs = 0;
+//unsigned int defaultfg = 15;
+//unsigned int defaultbg = 0;
+//static unsigned int defaultcs = 15;
+//static unsigned int defaultrcs = 0;
 
 /*
  * Default shape of cursor
